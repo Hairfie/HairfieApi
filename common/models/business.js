f9bdeb585d62d1709bec5d2448b2d41f565d4b3b
Antoine Hérault <antoine.herault@gmail.com>
2014-08-22 12:23:14 +0200

    Fix indentation

--- common/models/business.js
+++ common/models/business.js
@@ -3,109 +3,109 @@ var GeoPoint = require('loopback-datasource-juggler/lib/geo').GeoPoint;
 
 module.exports = function(Business) {
 
-  Business.nearby = function(here, page, limit, fn) {
-    if (typeof page === 'function') {
-      fn = page;
-      page = 0;
-      limit = 10;
-    }
-
-    if (typeof limit === 'function') {
-      fn = limit;
-      limit = 10;
-    }
-
-    var max = 1000;
-    page = page || 0;
-
-    var result = {};
-
-    async.series([
-        findBusinesses,
-        addDistance
-    ],
-    function(err){
-      fn(null, result);
-    });
-
-    function findBusinesses(cb) {
-      Business.find({
-        where: {gps: {near: here, maxDistance: max}},
-        // paging
-        skip: limit * page,
-        limit: limit
-      }, function(err, businesses) {
-        result = businesses;
-        cb(err);
-      });
-    }
-
-    function addDistance(cb) {
-      result.forEach(function(business) {
-          business.distance = GeoPoint.distanceBetween(here, business.gps, {type: 'meters'});
-      });
-      cb();
-    }
-  };
-
-  // Google Maps API has a rate limit of 10 requests per second
-  // Seems we need to enforce a lower rate to prevent errors
-  var lookupGeo = require('function-rate-limit')(5, 1000, function() {
-    var geoService = Business.app.dataSources.geo;
-    geoService.geocode.apply(geoService, arguments);
-  });
-
-  Business.beforeSave = function(next, business) {
-    if (business.gps) return next();
-    if(!business.street || !business.city || !business.zipcode) return next();
-
-    // geo code the address
-    lookupGeo(business.street, business.city, business.zipcode,
-      function(err, result) {
-        if (result && result[0]) {
-          business.gps = result[0].lng + ',' + result[0].lat;
-          next();
-        } else {
-          console.log('could not find location');
-          console.log(err);
-          next();
+    Business.nearby = function(here, page, limit, fn) {
+        if (typeof page === 'function') {
+            fn = page;
+            page = 0;
+            limit = 10;
         }
-      });
-  };
 
-  Business.setup = function() {
-    Business.base.setup.apply(this, arguments);
+        if (typeof limit === 'function') {
+            fn = limit;
+            limit = 10;
+        }
 
-    this.remoteMethod('nearby', {
-      description: 'Find nearby locations around you',
-      accepts: [
-        {arg: 'here', type: 'GeoPoint', required: true,
-          description: 'geo location:lng,lat. For ex : 2.30,48.87'},
-        {arg: 'page', type: 'Number',
-          description: 'number of pages (page size defined by limit)'},
-        {arg: 'limit', type: 'Number',
-          description: 'number of businesss to get, default=10'}
-      ],
-      returns: {arg: 'businesses', root: true},
-      http: { verb: 'GET' }
+        var max = 1000;
+        page = page || 0;
+
+        var result = {};
+
+        async.series([
+                findBusinesses,
+                addDistance
+                ],
+                function(err){
+                    fn(null, result);
+                });
+
+        function findBusinesses(cb) {
+            Business.find({
+                where: {gps: {near: here, maxDistance: max}},
+                // paging
+                skip: limit * page,
+                limit: limit
+            }, function(err, businesses) {
+                result = businesses;
+                cb(err);
+            });
+        }
+
+        function addDistance(cb) {
+            result.forEach(function(business) {
+                business.distance = GeoPoint.distanceBetween(here, business.gps, {type: 'meters'});
+            });
+            cb();
+        }
+    };
+
+    // Google Maps API has a rate limit of 10 requests per second
+    // Seems we need to enforce a lower rate to prevent errors
+    var lookupGeo = require('function-rate-limit')(5, 1000, function() {
+        var geoService = Business.app.dataSources.geo;
+        geoService.geocode.apply(geoService, arguments);
     });
-  };
 
-  Business.setup();
+    Business.beforeSave = function(next, business) {
+        if (business.gps) return next();
+        if(!business.street || !business.city || !business.zipcode) return next();
 
-  Business.prototype.gpsString = function() {
-    if(this.gps) {
-      return this.gps.lat+","+this.gps.lng;
-    } else {
-      return "";
-    }
-  };
+        // geo code the address
+        lookupGeo(business.street, business.city, business.zipcode,
+                function(err, result) {
+                    if (result && result[0]) {
+                        business.gps = result[0].lng + ',' + result[0].lat;
+                        next();
+                    } else {
+                        console.log('could not find location');
+                        console.log(err);
+                        next();
+                    }
+                });
+    };
 
-  Business.prototype.getStreetViewPic = function (width, height) {
-    width  = width || 400;
-    height = height || 400;
-    console.log(this.gpsString);
-    return "http://maps.googleapis.com/maps/api/streetview?size="+width+"x"+height+"&location="+this.gpsString();
-  };
+    Business.setup = function() {
+        Business.base.setup.apply(this, arguments);
+
+        this.remoteMethod('nearby', {
+            description: 'Find nearby locations around you',
+            accepts: [
+        {arg: 'here', type: 'GeoPoint', required: true,
+            description: 'geo location:lng,lat. For ex : 2.30,48.87'},
+            {arg: 'page', type: 'Number',
+                description: 'number of pages (page size defined by limit)'},
+            {arg: 'limit', type: 'Number',
+                description: 'number of businesss to get, default=10'}
+        ],
+            returns: {arg: 'businesses', root: true},
+            http: { verb: 'GET' }
+        });
+    };
+
+    Business.setup();
+
+    Business.prototype.gpsString = function() {
+        if(this.gps) {
+            return this.gps.lat+","+this.gps.lng;
+        } else {
+            return "";
+        }
+    };
+
+    Business.prototype.getStreetViewPic = function (width, height) {
+        width  = width || 400;
+        height = height || 400;
+        console.log(this.gpsString);
+        return "http://maps.googleapis.com/maps/api/streetview?size="+width+"x"+height+"&location="+this.gpsString();
+    };
 
 };
