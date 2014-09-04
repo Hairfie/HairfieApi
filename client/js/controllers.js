7ac2244081994688da0b01549b1035dfbb3d7c99
Antoine Hérault <antoine.herault@gmail.com>
2014-09-04 11:09:39 +0200

    Improve UX on reset password page

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -7,14 +7,20 @@ angular.module('myApp.controllers', [])
     .controller('ResetPasswordCtrl', ['$scope', '$routeParams', 'LoopBackAuth', 'User', function($scope, $routeParams, LoopBackAuth, User) {
         $scope.token = $routeParams.token;
         $scope.uid = $routeParams.uid;
+        $scope.displayForm = false;
 
         LoopBackAuth.currentUserId = $routeParams.uid;
         LoopBackAuth.accessTokenId = $routeParams.token;
         LoopBackAuth.save();
 
-        User.getCurrent(function () {}, function (res) {
-            $scope.errorMessage = 'The reset password token has expired, please try again.';
-        });
+        User.getCurrent(
+            function () {
+                $scope.displayForm = true;
+            },
+            function (res) {
+                $scope.errorMessage = 'The reset password token has expired, please try again.';
+            }
+        );
 
         $scope.changePassword = function() {
             $scope.ongoingProgress = true;
@@ -26,12 +32,13 @@ angular.module('myApp.controllers', [])
                 password: $scope.password
             }).$promise
             .then(function() {
-                $scope.successMessage = 'Success !';
+                $scope.successMessage = 'Your password has been successfully reset, you can now log in to the app using your new password.';
                 $scope.ongoingProgress = false;
+                $scope.displayForm = false;
             })
             .catch(function(error) {
 console.log(error);
-                $scope.errorMessage = 'Error !';
+                $scope.errorMessage = 'Your new password is not valid, please try again.';
                 $scope.ongoingProgress = false;
             });
         };
