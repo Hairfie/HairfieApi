51d637dec6f9b4ff703a87e105a97749b7e5bec8
Antoine Hérault <antoine.herault@gmail.com>
2014-09-04 10:13:10 +0200

    Style fix

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -3,35 +3,30 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('ResetPasswordCtrl', ['$scope', '$routeParams', 'LoopBackAuth', 'User', function($scope, $routeParams, LoopBackAuth, User) {
-    $scope.token = $routeParams.token;
-    $scope.uid = $routeParams.uid;
+    .controller('ResetPasswordCtrl', ['$scope', '$routeParams', 'LoopBackAuth', 'User', function($scope, $routeParams, LoopBackAuth, User) {
+        $scope.token = $routeParams.token;
+        $scope.uid = $routeParams.uid;
 
+        LoopBackAuth.currentUserId = $routeParams.uid;
+        LoopBackAuth.accessTokenId = $routeParams.token;
+        LoopBackAuth.save();
 
-    LoopBackAuth.currentUserId = $routeParams.uid;
-    LoopBackAuth.accessTokenId = $routeParams.token;
-    LoopBackAuth.save();
+        $scope.changePassword = function() {
+            $scope.ongoingProgress = true;
+            $scope.successMessage = null;
+            $scope.errorMessage = null;
 
-    $scope.changePassword = function() {
-      $scope.ongoingProgress = true;
-      $scope.successMessage = null;
-      $scope.errorMessage = null;
-
-      User.upsert({
-        id: $routeParams.uid,
-        password: $scope.password
-      }).$promise
-
-      .then(function() {
-          //alert('okay');
-          $scope.successMessage = 'Success !';
-          $scope.ongoingProgress = false;
-        })
-
-      .catch(function() {
-          //alert('not okay');
-          $scope.errorMessage = 'Error !';
-          $scope.ongoingProgress = false;
-        });
-    };
+            User.upsert({
+                id: $routeParams.uid,
+                password: $scope.password
+            }).$promise
+            .then(function() {
+                $scope.successMessage = 'Success !';
+                $scope.ongoingProgress = false;
+            })
+            .catch(function() {
+                $scope.errorMessage = 'Error !';
+                $scope.ongoingProgress = false;
+            });
+        };
   }]);
