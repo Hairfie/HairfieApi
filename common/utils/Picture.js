3bf4f7e8a54036972c10f30ace6882cf26d5c4cf
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-09-09 17:59:05 +0200

    fix public url

--- common/utils/Picture.js
+++ common/utils/Picture.js
@@ -5,9 +5,14 @@ function Picture(picture, container, appUrl) {
 }
 
 Picture.prototype.publicObject = function() {
+    if(this.picture.indexOf("http") == 0) {
+        var publicUrl = this.picture;
+    } else {
+        var publicUrl = this.appUrl + '/api/containers/' + this.container + '/download/' + this.picture;
+    }
     return {
         picture: this.picture,
-        publicUrl: this.appUrl + '/api/containers/' + this.container + '/download/' + this.picture
+        publicUrl: publicUrl
     }
 };
 
