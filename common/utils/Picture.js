bbdb6e14bfe4bbc448a8e435807ea01dc6637504
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-09-10 10:26:07 +0200

    fix

--- common/utils/Picture.js
+++ common/utils/Picture.js
@@ -5,11 +5,6 @@ function Picture(picture, container, appUrl) {
 }
 
 Picture.prototype.publicObject = function() {
-    if(this.picture && this.picture.indexOf("http") == 0) {
-        var publicUrl = this.picture;
-    } else {
-        var publicUrl = this.appUrl + '/api/containers/' + this.container + '/download/' + this.picture;
-    }
     return {
         picture: this.picture,
         publicUrl: publicUrl
