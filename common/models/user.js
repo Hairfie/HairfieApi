eac292951625cfc1dd5a50aafdddb9c740ae5569
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-09-23 21:00:34 +0200

    remove gravatar

--- common/models/user.js
+++ common/models/user.js
@@ -33,10 +33,10 @@ module.exports = function(User) {
 
     User.validatesInclusionOf('gender', {in: ['male', 'female']});
 
-    User.beforeSave = function(next, user) {
-        if (!user.picture) user.picture =  'http://www.gravatar.com/avatar/' + md5(user.email);
-        next();
-    }
+    // User.beforeSave = function(next, user) {
+    //     if (!user.picture) user.picture =  'http://www.gravatar.com/avatar/' + md5(user.email);
+    //     next();
+    // }
 
     User.afterCreate = function (next) {
         var user = this;
