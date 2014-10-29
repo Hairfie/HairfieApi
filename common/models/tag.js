98f91b4d10174029d2d6db126b75d1ce514e323f
Antoine Hérault <antoine.herault@gmail.com>
2014-10-29 11:41:29 +0100

    Fix tag

--- common/models/tag.js
+++ common/models/tag.js
@@ -11,7 +11,7 @@ module.exports = function (Tag) {
                 });
 
                 return obj;
-            });
+            }).bind(this));
     };
 
     Tag.prototype.toRemoteShortObject = function (context) {
