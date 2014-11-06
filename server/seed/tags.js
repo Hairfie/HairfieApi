8cf97838543d92f54e6e216bde13e83cb0a8fff6
Antoine Hérault <antoine.herault@gmail.com>
2014-11-06 16:52:34 +0100

    Add tags seed

---
+++ server/seed/tags.js
@@ -0,0 +1,34 @@
+var app = require('../..');
+var categoriesValues = require('./tags.json');
+var q = require('q');
+var TagCategory = app.models.TagCategory;
+var Tag = app.models.Tag;
+
+saveCategories(categoriesValues)
+    .then(function () {
+        console.log('Successfully seeded tags.');
+        process.exit(0);
+    })
+    .catch(function (error) {
+        console.log('Failed to seed tags:', error);
+        process.exit(1);
+    });
+
+function saveCategories(categoriesValues) {
+    return q.all(categoriesValues.map(saveCategory));
+}
+
+function saveCategory(categoryValues) {
+    return q.npost(TagCategory, 'create', [{name: categoryValues.name}])
+        .then(function (category) {
+            return saveTags(category, categoryValues.tags);
+        });
+}
+
+function saveTags(category, tagsValues) {
+    return q.all(tagsValues.map(saveTag.bind(null, category)));
+}
+
+function saveTag(category, tagValues) {
+    return q.npost(Tag, 'create', [{categoryId: category.id, name: tagValues.name}]);
+}
