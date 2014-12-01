9eefead51b00c7349e5ac84ae2c871a8f0038019
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-12-01 13:20:02 +0100

    add see managers

--- server/seed/managers.js
+++ server/seed/managers.js
@@ -1,17 +1,28 @@
-// var app = require('../..');
-// var q = require('q');
-// var Business = app.models.Business;
+var app = require('../..');
+var q = require('q');
+var Business = app.models.Business;
 
-// saveCategories()
-//     .then(function () {
-//         console.log('Successfully seeded tags.');
-//         process.exit(0);
-//     })
-//     .catch(function (error) {
-//         console.log('Failed to seed tags:', error);
-//         process.exit(1);
-//     });
+findClaimedBusinesses()
+    .then(function(businesses) {
+        return q.all(businesses.map(updateBusiness));
+    })
+    .then(function () {
+        console.log('Successfull.');
+        process.exit(0);
+    })
+    .catch(function (error) {
+        console.log('Failed to seed tags:', error);
+        process.exit(1);
+    });
 
-// function findClaimedBusinesses() {
-//     return q.npost(Business, );
-// }
+function findClaimedBusinesses() {
+    return q.ninvoke(Business, 'find', {where: {ownerId: {neq: null}}});
+}
+
+function updateBusiness(business) {
+    console.log("updateBusiness");
+    return q.npost(Business, 'update', [{
+        id       : business.id,
+        managerIds : [business.ownerId.toString()]
+    }]);
+}
