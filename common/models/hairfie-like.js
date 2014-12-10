5c321af4cddd69cbe91bd991ac0f504b254c2a01
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-12-10 14:49:02 +0100

    fix context and hairfielike

--- common/models/hairfie-like.js
+++ common/models/hairfie-like.js
@@ -3,12 +3,27 @@
 var Promise = require('../../common/utils/Promise');
 
 module.exports = function (HairfieLike) {
-    HairfieLike.prototype.toRemoteObject = function () {
+    HairfieLike.prototype.toRemoteObject = function (context) {
         return {
             hairfie : Promise.ninvoke(this.hairfie).then(function (hairfie) {
-                return hairfie ? hairfie.toRemoteObject() : null;
+                console.log("hairfie", hairfie);
+                return hairfie ? hairfie.toRemoteObject(context) : null;
             }),
             createdAt: this.createdAt
         };
     };
+
+    HairfieLike.validateAsync('userId', function (onError, onDone) {
+        this.user(function (error, user) {
+            if (error || !user) onError();
+            onDone();
+        });
+    }, {message: 'exists'});
+
+    HairfieLike.validateAsync('hairfieId', function (onError, onDone) {
+        this.hairfie(function (error, hairfie) {
+            if (error || !hairfie) onError();
+            onDone();
+        });
+    }, {message: 'exists'});
 };
