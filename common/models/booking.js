e58c91b7aa0f1104f53c057e7fda9faf4bb2b5e5
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-01-07 16:09:18 +0100

    add first version of booking

---
+++ common/models/booking.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var Promise = require('../../common/utils/Promise');
+
+module.exports = function (Booking) {
+    Booking.GENDER_MALE = 'MALE';
+    Booking.GENDER_FEMALE = 'FEMALE';
+};
