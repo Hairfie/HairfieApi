1c287011f83a28a45865751de72b918a9c500a0d
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-01-09 18:33:02 +0100

    add email notif

--- common/models/booking.js
+++ common/models/booking.js
@@ -5,4 +5,21 @@ var Promise = require('../../common/utils/Promise');
 module.exports = function (Booking) {
     Booking.GENDER_MALE = 'MALE';
     Booking.GENDER_FEMALE = 'FEMALE';
+
+
+    Booking.afterCreate = function (next) {
+        var Email = Booking.app.models.email;
+        var booking = this;
+
+        Promise.npost(this, 'business')
+            .then(function (business) {
+                return Business.app.models.email.notifySales('Demande de réservation', {
+                    'ID'       : booking.id,
+                    'Salon'    : business.name,
+                    'Tel du salon'    : business.phoneNumber,
+                    'Client'   : booking.firstName && booking.lastName
+                });
+            })
+            .then(next.bind(null, null), next);
+    };
 };
