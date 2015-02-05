d175f09ec6ceb649b5e3632e40fd53027b2e685f
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-02-05 11:07:40 +0100

    add places

---
+++ common/models/place.js
@@ -0,0 +1,18 @@
+var Promise = require('../../common/utils/Promise'),
+    Q = require('q'),
+    lodash = require('lodash');
+
+module.exports = function(Place) {
+    Place.prototype.toRemoteObject = function (context) {
+        return this.toRemoteShortObject();
+    };
+
+    Place.prototype.toRemoteShortObject = function (context) {
+        return {
+            id          : this.id,
+            name        : this.name,
+            description : this.description,
+            zipcodes    : this.zipcodes
+        };
+    };
+};
