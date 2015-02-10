b2e06260dd6ead9c3c9222ed4cd810dcc99225d3
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-02-10 10:22:00 +0100

    fix n

--- common/models/algolia-search-engine.js
+++ common/models/algolia-search-engine.js
@@ -39,7 +39,7 @@ module.exports = function (AlgoliaSearchEngine) {
         var Business = AlgoliaSearchEngine.app.models.Business;
         var deferred = Promise.defer();
         var index = getClient().initIndex(getBusinessIndex());
-        var limit = 1;
+        var limit = 50;
 
         function loop(skip) {
             if (progressHandler) progressHandler({done: skip});
