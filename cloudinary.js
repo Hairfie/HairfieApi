9c38f4101a1d582587a84a4f4941407251e760a5
Antoine Hérault <antoine.herault@gmail.com>
2015-04-08 16:11:27 +0200

    Add images import script

--- cloudinary.js
+++ cloudinary.js
@@ -5,112 +5,76 @@ var _ = require('lodash');
 var app = require('./');
 
 
-var Business = app.models.Business;
-var BusinessMember = app.models.BusinessMember;
+/**
+ * for each existing image
+ *      optimize
+ *      upload on new cloudinary
+ */
 
-return Q.ninvoke(model('Business'), 'findById', 'noop')
-    .then(step('Rename old images properties', parallel([
-        rename('User', 'picture'),
-        rename('Business', 'pictures'),
-        rename('BusinessMember', 'picture'),
-        rename('Hairfie', 'pictures'),
-        rename('Category', 'picture'),
-        rename('Place', 'picture')
-    ])))
-    .then(step('Update users', updateAll('User', 'picture', 'user-profile-pictures', 'users')))
-    .then(step('Update businesses', updateAll('Business', 'pictures', 'business-pictures', 'businesses')))
-    .then(step('Update business members', updateAll('BusinessMember', 'picture', 'business-pictures', 'business-members')))
-    .then(step('Update hairfies', updateAll('Hairfie', 'pictures', 'hairfies')))
-    .then(step('Update categories', updateAll('Category', 'picture', 'category', 'categories')))
-    .then(step('Update places', updateAll('Place', 'picture', 'places')))
-    .then(function () {
-        process.exit(0);
-    }, function (error) {
-        console.log(error);
-        process.exit(1);
+find(model('Image'))
+    .then(function (images) {
+        return throttle(images, 4, moveImage);
+    })
+    .then(
+        function () {
+            process.exit(0);
+        },
+        function (error) {
+            console.log(error);
+            process.exit(1);
+        }
+    );
+
+function moveImage(image) {
+    return model('AmazonS3Image')
+        .getDownloadUrl(image.container, image.id)
+        .then(model('KrakenImage').optimize)
+        .then(model('CloudinaryImage').uploadFromUrl.bind(model('CloudinaryImage'), image.container, image.id))
+        .catch(function (error) { console.log(image.id, 'failed:', error); });
+}
+
+function find(Model) {
+    var deferred = Q.defer();
+    Model.find({}, function (error, items) {
+        if (error) deferred.reject(error);
+        else deferred.resolve(items);
     });
-
+    return deferred.promise;
+}
 
 function model(name) { return app.models[name]; }
 function collection(Model) { return Model.dataSource.connector.collection(Model.definition.name); }
 
-function step(name, action) {
-    return function () {
-        console.log('----> '+name);
-        return Q(action());
-    };
-}
-
-function parallel(actions) {
-    return function () {
-        return Q.all(_.map(actions, function (a) { return a();}));
-    };
-}
-
-function rename(modelName, property) {
-    return function () {
-        var deferred = Q.defer();
-        var rename = {};
-        rename[property] = 'old_'+property;
-        collection(model(modelName)).update({}, {$rename: rename}, {multi: true}, function (error) {
-            if (error) deferred.reject(error);
-            else deferred.resolve();
-        });
-
-        return deferred.promise;
-    };
-}
-
-function update(property, oldContainer, newContainer) {
-    newContainer = newContainer || oldContainer;
-    return function (model) {
-        var one = 'picture' == property;
-        var old = model['old_'+property];
-        old = _.isArray(old) ? old : new Array(old);
-        old = _.filter(old, function (n) {
-            return _.isString(n) && !_.startsWith(n, 'http:');
-        });
-        if (one) old = old.slice(0, 1);
-
-        if (old.length == 0) return Q();
-
-        return Q.all(_.map(old, upload(oldContainer, newContainer)))
-            .then(function (files) {
-                if (one) model[property] = _.first(files);
-                else model[property] = files;
-
-                return Q.npost(model, 'save');
-            })
-            .catch(function (error) {
-                console.log(model.id, 'failed: ', error);
-            });
-    }
-}
-
-function updateAll(modelName, property, oldContainer, newContainer) {
-    var filter = {where: {}};
-    filter.where['old_'+property] = {exists: true, ne: null, not: {size: 0}};
-
-    return function () {
-        return find(modelName, filter).then(function (items) {
-            return Q.all(_.map(items, update(property, oldContainer, newContainer)));
-        });
-    }
-}
-
-function upload(oldContainer, newContainer) {
-    return function (oldName) {
-        return model('Image').uploadFromAmazonS3(oldContainer, oldName, newContainer);
-    };
-}
-
-function find(modelName, filter) {
+function throttle(values, max, iterator) {
+    max = max -1;
     var deferred = Q.defer();
+    var list = _.clone(values).reverse();
+    var outstanding = 0;
 
-    model(modelName).find(filter, function (error, results) {
-        if (error) deferred.reject(error);
-        else deferred.resolve(results);
-    });
+    function catchingFunction(value){
+        deferred.notify(value);
+        outstanding--;
+        if(list.length){
+            outstanding++;
+            iterator(list.pop())
+                .then(catchingFunction)
+                .fail(rejectFunction);
+        }
+        else if(outstanding===0){
+            deferred.resolve();
+        }
+    }
+
+    function rejectFunction(err) {
+        deferred.reject(err);
+    }
+
+    while(max-- && list.length){
+        iterator(list.pop())
+            .then(catchingFunction)
+            .fail(rejectFunction);
+        outstanding++;
+    }
 
     return deferred.promise;
-}
+};
