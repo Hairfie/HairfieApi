2dff23e8cc5870c68492698e01828e0d3c1fdb87
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-06-09 14:15:23 +0200

    fix

---
+++ common/models/text-message.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var Q = require('q');
+var twilio = require('twilio');
+
+module.exports = function (TextMessage) {
+    var client;
+
+    TextMessage.on('dataSourceAttached', function () {
+        client = new twilio.RestClient(TextMessage.dataSource.settings.twilioAccountSID, TextMessage.dataSource.settings.twilioAuthToken);
+    });
+
+    TextMessage.send = function (toNumber, body) {
+        var env = TextMessage.app.get('env');
+
+        var envLabel = (env.toLowerCase() !== 'production') ? '[' + env + '] ' : ' ';
+        console.log("env", env);
+        console.log("envLabel", envLabel);
+
+        return client.sendMessage({
+            to: toNumber,
+            from: TextMessage.dataSource.settings.twilioNumber || "+1 415-599-2671",
+            body: envLabel + body
+        }).then(function(responseData) {
+            console.log('Successfully send message', responseData);
+            return responseData;
+        }).fail(function(error) {
+            console.log("error", error);
+            return error;
+        });
+
+    };
+};
