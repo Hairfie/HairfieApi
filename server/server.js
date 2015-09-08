c5f743354afdb1a26bb1dd8a5f9e213da2f9a72e
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-09-08 16:53:33 +0200

    logs

--- server/server.js
+++ server/server.js
@@ -35,7 +35,9 @@ if (!passportConfig) {
 //    model: app.models.AccessToken
 //}));
 app.use(function (req, res, next) {
-    console.log("here, app.use", req.accessToken);
+    console.log("here, app.use", req.body);
+        console.log("here, app.use", req.body.toString());
+
 
     // temporary replace token middleware
     if (req.accessToken) return next();
