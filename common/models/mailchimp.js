385271f71b095dc72995ab3c4e60ab4b7aed6b0f
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-09-28 16:26:30 +0200

    workinprogress

--- common/models/mailchimp.js
+++ common/models/mailchimp.js
@@ -2,6 +2,7 @@
 
 var Q = require('q');
 var MailchimpLite = require('mailchimp-lite');
+var _ = require('lodash');
 
 module.exports = function (Mailchimp) {
     var client;
@@ -13,6 +14,37 @@ module.exports = function (Mailchimp) {
         });
     });
 
+    function formatUserForMailchimp(user) {
+        return {
+            email: {email: user.email},
+            merge_vars: {
+                fname: user.firstName,
+                lname: user.lastName,
+                registered: "YES"
+            }
+        }
+    }
+
+    Mailchimp.updateAllUser = function () {
+        var listId = Mailchimp.dataSource.settings.listId;
+        var User = Mailchimp.app.models.User;
+
+        return Q.ninvoke(User, 'find', {})
+        .then(function(users) {
+            return client.v2.post('/lists/batch-subscribe', {
+                id: listId,
+                update_existing: true,
+                double_optin: false,
+                replace_interests: false,
+                batch: _.map(users, formatUserForMailchimp)
+            })
+        })
+        .then((response) => { console.log(response); })
+        .catch((error) => {
+            console.log(error); // Mailchimp Error: 401
+            console.log(error.response);
+        });
+    };
 
     Mailchimp.addToList = function (subscriber) {
         var listId = Mailchimp.dataSource.settings.listId;
