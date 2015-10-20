3f787c7d69d35a373a9e67fec5435835ecb21b49
Bastien Chevallier <bastien@hairfie.com>
2015-10-20 18:00:27 +0200

    script

---
+++ server/seed/isClaimed.js
@@ -0,0 +1,40 @@
+var app = require('../..');
+var q = require('q');
+var Business = app.models.Business;
+var lodash = require('lodash');
+
+findBusiness(24800)
+    .then(function (results) {
+        console.log('Successfull.');
+        process.exit(0);
+    })
+    .catch(function (error) {
+        console.log('Failed to seed tags:', error);
+        process.exit(1);
+    });
+
+function findBusiness(skip) {
+    skip = skip || 0;
+    return q.ninvoke(Business, 'find', {limit: 100, skip: skip})
+        .then(function(businesses) {
+            console.log("Found : ", (businesses.length + skip));
+            return q.all(businesses.map(updateBusiness))
+                .then(function() {
+                    return findBusiness(skip + 100);
+                });
+        });
+}
+
+function updateBusiness(business) {
+    if (business && business.accountType == "FREE") {
+        return business.isClaimed()
+            .then(function(results) {
+                if (results && business) {
+                    return q.ninvoke(business, 'updateAttributes', {accountType: "BASIC"});
+                }
+                else {
+                    return;
+                }
+        });
+    }
+}
