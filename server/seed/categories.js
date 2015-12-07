ba146661b415c3d864953fd4a8ab4168d93fb27c
Bastien Chevallier <bastien@hairfie.com>
2015-12-07 16:23:21 +0100

    categories

--- server/seed/categories.js
+++ server/seed/categories.js
@@ -27,7 +27,7 @@ function saveCategories(categoriesDefinitions) {
 
 function saveCategory(categoryDefinition, position) {
     return q.all(categoryDefinition.tagNames.map(function(tag) {
-        return q.ninvoke(Tag, 'find', {})
+        return q.ninvoke(Tag, 'find', {where: {"name.fr": tag}})
     }))
     .then(function(tags) {
         console.log(tags.length + " tags trouvés avec la requête " + categoryDefinition.tagNames);
