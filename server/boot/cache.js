bdaa667df58d5c9cea4abf7fb00881ab8745cc23
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-12-08 09:35:07 +0100

    add similar businesses

--- server/boot/cache.js
+++ server/boot/cache.js
@@ -7,7 +7,15 @@ module.exports = function (app) {
         host: app.get('redisHost'),
         port: app.get('redisPort'),
         auth_pass: app.get('redisPassword'),
-        prefix: app.get('redisPrefix')
+        prefix: app.get('redisPrefix'),
+        expire: {
+            200: 5000,
+            404: 10,
+            401: 1,
+            403: 5000,
+            500: 1,
+            503: 1
+        }
     });
 
     app.get('/*/blogPosts', client.route({ expire: 10000, type: "application/json" }), function (req, res, next) {
@@ -32,5 +40,14 @@ module.exports = function (app) {
 
     app.get('/*/hairfies/similar-hairfies', client.route({ expire: 100000, type: "application/json" }), function (req, res, next) {
         next();
-    })
+    });
+
+    app.get('/*/businesses/:businessId/similar', function (req, res, next) {
+        res.express_redis_cache_name = 'businesses-similar-' + req.params.businessId;
+        next();
+    });
+
+    app.get('/*/businesses/:buisnessId/similar', client.route({ expire: 100000, type: "application/json" }), function (req, res, next) {
+        next();
+    });
 };
