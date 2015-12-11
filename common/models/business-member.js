6c6768a643b38e407cc2dcefef89607f0118a67c
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-12-11 15:09:16 +0100

    try pm2

--- common/models/business-member.js
+++ common/models/business-member.js
@@ -71,6 +71,7 @@ module.exports = function (BusinessMember) {
     BusinessMember.prototype.getNumHairfies = function (cb) {
         if (!this.id) cb(null, 0);
         var Hairfie = BusinessMember.app.models.Hairfie;
+        console.log("getNumHairfies");
         Hairfie.count({businessMemberId: this.id}, cb);
     };
 
